9245a19ebaea326b4a8cb79f303671cef3293b15
kevvo15 <58575080+kevvo15@users.noreply.github.com>
2020-11-03 16:51:17 -0700

    Updated GUI Events
    - Most CRUD operations are complete and function within its own database.
    - Create, Read, and Delete function properly (on its own database)
    - Update method still needs to be worked on
    - Simplified code to be less redundant; will continue to be simplified in future commits
    - NOT final

--- src/main/java/com/dreamteam/database/GUI.java
+++ src/main/java/com/dreamteam/database/GUI.java
@@ -4,51 +4,60 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.EnumSet;
 
 public class GUI implements ActionListener {
 
+    //Class member variables
     private JFrame frame;
+    private JFrame frame2;
     private JPanel panel;
-    private JLabel label;
+    private JPanel panel2;
+    private ProductDatabase database = ProductDatabase.getProducts();
+
+    //Necessary labels for most/all methods
+    JLabel productLabel = new JLabel("Product ID:");
+    JLabel quantityLabel = new JLabel("Quantity:");
+    JLabel wholesaleLabel = new JLabel("Wholesale Cost:");
+    JLabel salesLabel = new JLabel("Sales Price:");
+    JLabel supplierLabel = new JLabel("Supplier ID:");
+
+    //Globalized text area components to store user input
+    JTextArea product;
+    JTextArea quantity;
+    JTextArea wholesale;
+    JTextArea sales;
+    JTextArea supplier;
+    JTextArea date;
+    JButton submit;
+
 
     public GUI() {
 
         //Create window frame
         frame = new JFrame();
+        JLabel label = new JLabel("What operation would you like to perform?");
 
-        //Create label and button components
-        label = new JLabel("What operation would you like to perform?");
-        JButton create = new JButton("CREATE");
-        JButton read = new JButton("READ");
-        JButton update = new JButton("UPDATE");
-        JButton delete = new JButton("DELETE");
-        JButton process = new JButton("PROCESS ORDERS");
-        JButton reports = new JButton("REPORTS");
-        JButton quit = new JButton("QUIT");
-
-        //Action Events for buttons
-        create.addActionListener(this::actionPerformed);
-        read.addActionListener(this::actionPerformed);
-        update.addActionListener(this::actionPerformed);
-        delete.addActionListener(this::actionPerformed);
-        process.addActionListener(this::actionPerformed);
-        reports.addActionListener(this::actionPerformed);
-        quit.addActionListener(this::actionPerformed);
+        //Create button components
+        final EnumSet<Options> OPTIONS = EnumSet.of(Options.CREATE, Options.READ, Options.UPDATE, Options.DELETE, Options.PROCESS_ORDERS, Options.REPORTS, Options.QUIT);
+        ArrayList<JButton> buttonList = new ArrayList<JButton>();
+        for(Options option : OPTIONS){
+            buttonList.add(new JButton(String.valueOf(option)));
+        }
 
         //Create panel and configure size
         panel = new JPanel();
+        panel.add(label);
         panel.setBorder(BorderFactory.createEmptyBorder(125,300,100,300));
         panel.setLayout(new GridLayout(0,1));
 
-        //Add components to panel
-        panel.add(label);
-        panel.add(create);
-        panel.add(read);
-        panel.add(update);
-        panel.add(delete);
-        panel.add(process);
-        panel.add(reports);
-        panel.add(quit);
+        //Insert buttons onto GUI panel
+        for(int i = 0; i < buttonList.size(); i++){
+            buttonList.get(i).addActionListener(this::actionPerformed);
+            panel.add(buttonList.get(i));
+        }
+
 
         //Attach panel to frame
         frame.add(panel, BorderLayout.CENTER);
@@ -59,29 +68,124 @@ public class GUI implements ActionListener {
 
     }
 
+
     @Override
     public void actionPerformed(ActionEvent e) {
+
+        //Button events open new window
+        frame2 = new JFrame();
+        panel2 = new JPanel();
+        panel2.setBorder(BorderFactory.createEmptyBorder(125,300,100,300));
+        panel2.setLayout(new GridLayout(0,1));
+
+
         if(e.getActionCommand() == "CREATE"){
-            label.setText("create clicked");
+
+            product = new JTextArea();
+            quantity = new JTextArea();
+            wholesale = new JTextArea();
+            sales = new JTextArea();
+            supplier = new JTextArea();
+
+            submit = new JButton("SUBMIT");
+            submit.addActionListener(this::createSubmit);
+
+            panel2.add(productLabel);
+            panel2.add(product);
+            panel2.add(quantityLabel);
+            panel2.add(quantity);
+            panel2.add(wholesaleLabel);
+            panel2.add(wholesale);
+            panel2.add(salesLabel);
+            panel2.add(sales);
+            panel2.add(supplierLabel);
+            panel2.add(supplier);
+            panel2.add(submit);
+
+            newGUI("CREATE");
         }
+
         else if (e.getActionCommand() == "READ"){
-            label.setText("read clicked");
+            product = new JTextArea();
+            panel2.add(productLabel);
+            panel2.add(product);
+            submit = new JButton("SUBMIT");
+            submit.addActionListener(this::readSubmit);
+            panel2.add(submit);
+            newGUI("READ");
         }
+
         else if (e.getActionCommand() == "UPDATE"){
-            label.setText("update clicked");
+
+            EnumSet<Options> OPTIONS = EnumSet.of(Options.QUANTITY, Options.CAPACITY, Options.WHOLESALE_COST, Options.SALE_PRICE, Options.SUPPLIER, Options.DONE);
+            ArrayList<JButton> buttonList = new ArrayList<JButton>();
+            for(Options option : OPTIONS){
+                buttonList.add(new JButton(String.valueOf(option)));
+            }
+            for(int i = 0; i < buttonList.size(); i++){
+                panel2.add(buttonList.get(i));
+            }
+            newGUI("UPDATE");
         }
+
         else if (e.getActionCommand() == "DELETE"){
-            label.setText("delete clicked");
+            product = new JTextArea();
+            panel2.add(productLabel);
+            panel2.add(product);
+            submit = new JButton("SUBMIT");
+            submit.addActionListener(this::deleteSubmit);
+            panel2.add(submit);
+            newGUI("DELETE");
         }
-        else if (e.getActionCommand() == "PROCESS ORDERS"){
-            label.setText("process clicked");
+
+        else if (e.getActionCommand() == "PROCESS_ORDERS"){
+            JLabel processLabel = new JLabel("Order Processing NOT Complete!");
+            panel2.add(processLabel);
+            newGUI("PROCESS ORDERS");
         }
+
         else if (e.getActionCommand() == "REPORTS"){
-            label.setText("reports clicked");
+            JLabel reportsLabel = new JLabel("For which date would you like reports for?");
+            date = new JTextArea();
+            panel2.add(reportsLabel);
+            panel2.add(date);
+            newGUI("REPORTS");
         }
+
         else if (e.getActionCommand() == "QUIT"){
             System.exit(0);
         }
+    }
+
+    public void createSubmit(ActionEvent e) {
+        String product_id = product.getText();
+        int product_quantity = Integer.parseInt(quantity.getText());
+        double wholesale_cost = Double.parseDouble(wholesale.getText());
+        double sales_price = Double.parseDouble(sales.getText());
+        String supplier_id = supplier.getText();
+
+        Product newProduct = new Product(product_id, product_quantity, wholesale_cost,sales_price,supplier_id);
+        database.create(newProduct);
+
+        System.out.println("Product created!");
+    }
+
+    public void readSubmit(ActionEvent e) {
+        String product_id = product.getText();
+        System.out.println(database.read(product_id));
+    }
+
+    public void deleteSubmit(ActionEvent e) {
+        String product_id = product.getText();
+        database.delete(product_id);
+        System.out.println("Product deleted successfully!");
+    }
+
+    public void newGUI(String title) {
+        frame2.setTitle("DATABASE OPERATION: " + title);
+        frame2.add(panel2, BorderLayout.CENTER);
+        frame2.pack();
+        frame2.setVisible(true);
     }
 
     public static void main(String[] args) {
