4120c329b059dc7acc4997f49ed6a7bee3f0c53e
kevvo15 <58575080+kevvo15@users.noreply.github.com>
2020-11-03 12:52:32 -0700

    Added ActionEvents
    I mimplemented ActionListener so I can get all the button have some functionality when clicked. They are ready to be connected to program.

--- src/main/java/com/dreamteam/database/GUI.java
+++ src/main/java/com/dreamteam/database/GUI.java
@@ -2,16 +2,22 @@ package com.dreamteam.database;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI {
+public class GUI implements ActionListener {
+
+    private JFrame frame;
+    private JPanel panel;
+    private JLabel label;
 
     public GUI() {
 
         //Create window frame
-        JFrame frame = new JFrame();
+        frame = new JFrame();
 
         //Create label and button components
-        JLabel label = new JLabel("What operation would you like to perform?");
+        label = new JLabel("What operation would you like to perform?");
         JButton create = new JButton("CREATE");
         JButton read = new JButton("READ");
         JButton update = new JButton("UPDATE");
@@ -20,9 +26,18 @@ public class GUI {
         JButton reports = new JButton("REPORTS");
         JButton quit = new JButton("QUIT");
 
+        //Action Events for buttons
+        create.addActionListener(this::actionPerformed);
+        read.addActionListener(this::actionPerformed);
+        update.addActionListener(this::actionPerformed);
+        delete.addActionListener(this::actionPerformed);
+        process.addActionListener(this::actionPerformed);
+        reports.addActionListener(this::actionPerformed);
+        quit.addActionListener(this::actionPerformed);
+
         //Create panel and configure size
-        JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(150,300,100,300));
+        panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(125,300,100,300));
         panel.setLayout(new GridLayout(0,1));
 
         //Add components to panel
@@ -42,6 +57,31 @@ public class GUI {
         frame.pack();
         frame.setVisible(true);
 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getActionCommand() == "CREATE"){
+            label.setText("create clicked");
+        }
+        else if (e.getActionCommand() == "READ"){
+            label.setText("read clicked");
+        }
+        else if (e.getActionCommand() == "UPDATE"){
+            label.setText("update clicked");
+        }
+        else if (e.getActionCommand() == "DELETE"){
+            label.setText("delete clicked");
+        }
+        else if (e.getActionCommand() == "PROCESS ORDERS"){
+            label.setText("process clicked");
+        }
+        else if (e.getActionCommand() == "REPORTS"){
+            label.setText("reports clicked");
+        }
+        else if (e.getActionCommand() == "QUIT"){
+            System.exit(0);
+        }
     }
 
     public static void main(String[] args) {
