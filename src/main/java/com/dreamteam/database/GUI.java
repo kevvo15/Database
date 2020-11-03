944511fdc0e668efe699a00b443abae2c17a7ed1
kevvo15 <58575080+kevvo15@users.noreply.github.com>
2020-11-03 09:56:16 -0700

    GUI Setup
    Created simple GUI using main method. NOT FINAL

--- src/main/java/com/dreamteam/database/GUI.java
+++ src/main/java/com/dreamteam/database/GUI.java
@@ -1,4 +1,28 @@
 package com.dreamteam.database;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class GUI {
+
+    public GUI() {
+        JFrame frame = new JFrame();
+
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
+        panel.setLayout(new GridLayout(0,1));
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("DREAMTEAM DATABASES");
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+
+        new GUI();
+
+    }
+
 }
